8838eb652ec2e30bd351a88296728d8dc5d4cfc1
Juan Carlos Dominguez <jcarlosdominguez38@gmail.com>
2024-02-09 22:32:03 -0800

    height

--- index.js
+++ index.js
@@ -1,24 +1,27 @@
-$(document).ready(function() {
+$(document).ready(function () {
     var envelope = $("#envelope");
     var btn_open = $("#open");
     var btn_reset = $("#reset");
-    var btn_yes = $(".yes-btn"); // Make sure to select the correct button
+    var btn_yes = $(".yes-btn");
 
-    envelope.click(function() {
+    envelope.click(function () {
         open();
     });
 
-    btn_open.click(function() {
+    btn_open.click(function () {
         open();
     });
 
-    btn_reset.click(function() {
+    btn_reset.click(function () {
         close();
     });
 
-    btn_yes.click(function() {
+    btn_yes.click(function () {
         showHeart(btn_yes);
         highlightYesButton();
+        // Add your additional effects and logic here
+        // For example, you can show a message, play a sound, or redirect to another page
+        alert("Thank you for saying Yes!");
     });
 
     function open() {
@@ -28,4 +31,15 @@ $(document).ready(function() {
     function close() {
         envelope.addClass("close").removeClass("open");
     }
+
+    function showHeart(button) {
+        // Implement the logic to show hearts or any other effects
+        // You can customize this function based on your requirements
+    }
+
+    function highlightYesButton() {
+        // Implement the logic to highlight the "Yes" button
+        // You can customize this function based on your requirements
+    }
 });
+
