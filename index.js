1e4b293d4d150b88ea70cb32a001e913c1c25371
Juan Carlos Dominguez <jcarlosdominguez38@gmail.com>
2024-02-09 22:00:01 -0800

    particles

--- index.js
+++ index.js
@@ -2,25 +2,30 @@ $(document).ready(function() {
     var envelope = $("#envelope");
     var btn_open = $("#open");
     var btn_reset = $("#reset");
+    var btn_yes = $(".yes-btn"); // Make sure to select the correct button
 
     envelope.click(function() {
         open();
     });
+
     btn_open.click(function() {
         open();
     });
+
     btn_reset.click(function() {
         close();
     });
 
+    btn_yes.click(function() {
+        showHeart(btn_yes);
+        highlightYesButton();
+    });
+
     function open() {
-        envelope.addClass("open")
-            .removeClass("close");
+        envelope.addClass("open").removeClass("close");
     }
 
     function close() {
-        envelope.addClass("close")
-            .removeClass("open");
+        envelope.addClass("close").removeClass("open");
     }
-
-})
+});
